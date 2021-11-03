d711c39d6559503bde115a1345fb3e7b2fca3408
treatme030 <treatme8192@gmail.com>
2021-11-03 18:41:26 +0900

    product ctrl, route 작성

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,7 @@
 import Product from "../models/product";
 
+
+//create product
 export const createProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(201).json({
@@ -8,6 +10,64 @@ export const createProduct = async (req, res, next) => {
     })
 }
 
-export const getAllProducts = (req, res) => {
-    res.status(200).json({ message: "Route is working fine"})
+//get all products
+export const getAllProducts = async (req, res) => {
+    const products = await Product.find();
+    res.status(200).json({
+        success: true,
+        products,
+    })
+}
+
+//get product details
+export const getProductDetails = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found",
+        })
+    }
+    res.status(200).json({
+        success: true,
+        product,
+    })
+}
+
+//update product 
+export const updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found",
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    res.status(200).json({
+        success: true,
+        product,
+    })
+}
+
+//delete product
+export const deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product){
+        return res.status(500).json({
+            success: false,
+            message: "Product not found",
+        })
+    }
+    await product.remove();
+    res.status(200).json({
+        success: true,
+        message: "Product delete successfully",
+    })
 }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { createProduct, getAllProducts } from '../controllers/productController';
+import * as ctrl from '../controllers/productController';
 
 const router = express.Router();
 
-router.route("/products").get(getAllProducts);
-router.route("/products/new").post(createProduct);
+router.route("/products").get(ctrl.getAllProducts);
+router.route("/products/new").post(ctrl.createProduct);
+router.route("/products/:id").put(ctrl.updateProduct).delete(ctrl.deleteProduct).get(ctrl.getProductDetails);
+
 
 export default router;

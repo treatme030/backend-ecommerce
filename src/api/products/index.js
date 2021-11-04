60ed1385898157f7bf2843b2958499668d9919d6
treatme030 <treatme8192@gmail.com>
2021-11-04 22:28:38 +0900

    ObjectId 확인 함수 추가

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -3,18 +3,10 @@ import * as productsCtrl from './products.ctrl';
 
 const products = new Router;
 
-const printInfo = ctx => {
-    ctx.body = {
-        method: ctx.method,
-        path: ctx.path,
-        params: ctx.params,
-    }
-}
-
-products.get('/', productsCtrl.items)
+products.get('/', productsCtrl.list)
 products.post('/', productsCtrl.create)
-products.patch('/:id', productsCtrl.update)
-products.delete('/:id', productsCtrl.remove)
-products.get('/:id', productsCtrl.select)
+products.patch('/:id', productsCtrl.getProductById, productsCtrl.update)
+products.delete('/:id', productsCtrl.getProductById, productsCtrl.remove)
+products.get('/:id', productsCtrl.getProductById, productsCtrl.select)
 
 export default products;
--- src/api/products/products.ctrl.js
+++ src/api/products/products.ctrl.js
@@ -1,9 +1,31 @@
 import mongoose from 'mongoose';
 import Product from '../../models/product';
 
+const { ObjectId } = mongoose.Types;
+
+//클라이언트가 요청을 잘못 보낸건지 ObjectId 확인
+export const getProductById = async (ctx, next) => {
+    const { id } = ctx.params;
+    if (!ObjectId.isValid(id)) {
+        ctx.status = 400;//bad request
+        return;
+    }
+    try {
+        const product = await Product.findById(id);
+        if (!product) {
+            ctx.status = 404;
+            return;
+        }
+        ctx.state.product = product;
+        return next();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+}
+
 //모든 상품 불러오기
 //GET --> /api/products
-export const items = async ctx => {
+export const list = async ctx => {
     try {
         //find() 함수를 호출한 후에 exec()를 붙여 주어야 서버에 쿼리를 요청함
         const products = await Product.find().exec();
@@ -40,7 +62,6 @@ export const create = async ctx => {
 //PATCH --> api/products/:id
 export const update = async ctx => {
     const { id } = ctx.params;
-    console.log(ctx.request.body)
     try {
         const product = await Product.findByIdAndUpdate(id, ctx.request.body, {
             new: true, //true:업데이트된 데이터 반환, false:업데이트되기 전 데이터 반환 
@@ -70,15 +91,5 @@ export const remove = async ctx => {
 //특정 상품 보기
 //GET --> api/products/:id
 export const select = async ctx => {
-    const { id } = ctx.params;
-    try {
-        const product = await Product.findById(id).exec();
-        if(!product){
-            ctx.status = 404;
-            return;
-        }
-        ctx.body = product;
-    } catch(e){
-        ctx.throw(500, e);
-    }
+    ctx.body = ctx.state.product;
 }
